d6ee989ccc100b3a6ef1d87266b1dfa724a55426
eduardocaas <eduardofc.dev@gmail.com>
2024-06-21 19:41:57 -0300

    Adicionadas propriedades

--- Models/Nota.cs
+++ Models/Nota.cs
@@ -2,5 +2,19 @@
 {
     public class Nota
     {
+        public int Id { get; set; }
+        public int Numero { get; set; }
+        public DateTime DataEmissao { get; set; }
+        public DateTime Garantia { get; set; }
+        public float ValorVenda { get; set; }
+
+        public int CompradorId { get; set; }
+        public Cliente Comprador { get; set; }
+
+        public int VendedorId { get; set; }
+        public Vendedor Vendedor { get; set; }
+
+        public int CarroId { get; set; }
+        public Carro Carro { get; set; }
     }
 }
