cfaa01f756ed39faf0e9c0ff22bd3bc1da72125b
eduardocaas <eduardofc.dev@gmail.com>
2024-06-21 19:40:01 -0300

    Adicionadas propriedades

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -2,5 +2,12 @@
 {
     public class Cliente
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public string CPF { get; set; }
     }
 }
