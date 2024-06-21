3b3c73ec85f6bc79a3bd0cb09440d9a23a22e01b
eduardocaas <eduardofc.dev@gmail.com>
2024-06-21 19:41:07 -0300

    Adicionadas propriedades

--- Models/Vendedor.cs
+++ Models/Vendedor.cs
@@ -2,5 +2,10 @@
 {
     public class Vendedor
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataAdmissao { get; set; }
+        public string Matricula { get; set; }
+        public float Salario { get; set; }
     }
 }
