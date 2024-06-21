f5561fac3c026026903768b8e7b6edb08e3ce475
eduardocaas <eduardofc.dev@gmail.com>
2024-06-21 19:40:29 -0300

    Validação de email

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,10 +1,13 @@
-﻿namespace CarCenter.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarCenter.Models
 {
     public class Cliente
     {
         public int Id { get; set; }
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public string Telefone { get; set; }
         public string Endereco { get; set; }
