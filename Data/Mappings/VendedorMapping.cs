80f421eda8f508167b4dec5e336307387b50e470
eduardocaas <eduardofc.dev@gmail.com>
2024-06-21 22:24:19 -0300

    Implementado mapeamento

--- Data/Mappings/VendedorMapping.cs
+++ Data/Mappings/VendedorMapping.cs
@@ -1,6 +1,38 @@
-﻿namespace CarCenter.Data.Mappings
+﻿using CarCenter.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarCenter.Data.Mappings
 {
-    public class VendedorMapping
+    public class VendedorMapping : IEntityTypeConfiguration<Vendedor>
     {
+        public void Configure(EntityTypeBuilder<Vendedor> builder)
+        {
+            builder
+                .ToTable("Vendedor")
+                .HasKey(v => v.Id);
+
+            builder
+                .Property(v => v.Nome)
+                .IsRequired()
+                .HasColumnType("VARCHAR");
+
+            builder
+                .Property(v => v.DataAdmissao)
+                .IsRequired()
+                .HasColumnType("DATETIME")
+                .HasColumnName("Data_Admissao")
+                .HasDefaultValue(DateTime.Now);
+
+            builder
+                .Property(v => v.Matricula)
+                .IsRequired()
+                .HasColumnType("VARCHAR");
+
+            builder
+                .Property(v => v.Salario)
+                .IsRequired()
+                .HasColumnType("FLOAT");
+        }
     }
 }
