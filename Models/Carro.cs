4e427c758f8e0741cf22758450be5a4a20560ec6
eduardocaas <eduardofc.dev@gmail.com>
2024-06-21 23:59:58 -0300

    Adicionada mensagem customizada para Required

--- Models/Carro.cs
+++ Models/Carro.cs
@@ -6,16 +6,22 @@ namespace CarCenter.Models
     {
         public int Id { get; set; }
         [MaxLength(80, ErrorMessage = "Marca muito longa")]
+        [Required(ErrorMessage = "Required")]
         public string Marca { get; set; }
         [MaxLength(80, ErrorMessage = "Modelo muito longo")]
+        [Required(ErrorMessage = "Required")]
         public string Modelo { get; set; }
         [Display(Name = "Ano do Modelo")]
+        [Required(ErrorMessage = "Required")]
         public int AnoModelo { get; set; }
         [Display(Name = "Ano de Fabricação")]
+        [Required(ErrorMessage = "Required")]
         public int AnoFabricacao { get; set; }
         [MaxLength(80, ErrorMessage = "Chassi muito longo")]
+        [Required(ErrorMessage = "Required")]
         public string Chassi { get; set; }
         [DisplayFormat(DataFormatString = "{0:#,##0.00}")]
+        [Required(ErrorMessage = "Required")]
         public float Preco { get; set; }
     }
 }
