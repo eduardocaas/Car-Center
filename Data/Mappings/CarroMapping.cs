d63edff7b5c47b309892ec0db8a77f418edf2086
eduardocaas <eduardofc.dev@gmail.com>
2024-06-21 22:23:10 -0300

    Adicionado CarroMapping.cs

---
+++ Data/Mappings/CarroMapping.cs
@@ -0,0 +1,14 @@
+﻿using CarCenter.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarCenter.Data.Mappings
+{
+    public class CarroMapping : IEntityTypeConfiguration<Carro>
+    {
+        public void Configure(EntityTypeBuilder<Carro> builder)
+        {
+            
+        }
+    }
+}
