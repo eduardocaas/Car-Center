60aa63f2a50735911839b4cf62bdad658d89f3b3
eduardocaas <eduardofc.dev@gmail.com>
2024-06-21 22:26:13 -0300

    Implementado mapeamento

--- Data/Mappings/NotaMapping.cs
+++ Data/Mappings/NotaMapping.cs
@@ -1,6 +1,38 @@
-﻿namespace CarCenter.Data.Mappings
+﻿using CarCenter.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarCenter.Data.Mappings
 {
-    public class NotaMapping
+    public class NotaMapping : IEntityTypeConfiguration<Nota>
     {
+        public void Configure(EntityTypeBuilder<Nota> builder)
+        {
+            builder
+                .ToTable("Nota")
+                .HasKey(n => n.Id);
+
+            builder
+                .Property(n => n.Numero)
+                .IsRequired()
+                .HasColumnType("INT");
+
+            builder
+                .Property(n => n.DataEmissao)
+                .HasColumnType("DATETIME")
+                .HasColumnName("Data_Emissao")
+                .HasDefaultValue(DateTime.Now);
+
+            builder
+                .Property(n => n.Garantia)
+                .HasColumnType("DATETIME")
+                .HasDefaultValue(DateTime.Now.AddYears(2)); // Definido 2 anos de garantia
+
+            builder
+                .Property(n => n.ValorVenda)
+                .IsRequired()
+                .HasColumnType("FLOAT")
+                .HasColumnName("Valor_Venda");
+        }
     }
 }
