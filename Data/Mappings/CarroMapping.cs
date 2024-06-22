68dfb6e50b6cee9c77f1294eca57e7787a2ffcd3
eduardocaas <eduardofc.dev@gmail.com>
2024-06-21 22:23:24 -0300

    Implementado mapeamento

--- Data/Mappings/CarroMapping.cs
+++ Data/Mappings/CarroMapping.cs
@@ -8,7 +8,41 @@ namespace CarCenter.Data.Mappings
     {
         public void Configure(EntityTypeBuilder<Carro> builder)
         {
-            
+            builder
+                .ToTable("Carro")
+                .HasKey(c => c.Id);
+
+            builder
+                .Property(c => c.Marca)
+                .IsRequired()
+                .HasColumnType("VARCHAR");
+
+            builder
+                .Property(c => c.Modelo)
+                .IsRequired()
+                .HasColumnType("VARCHAR");
+
+            builder
+                .Property(c => c.AnoModelo)
+                .IsRequired()
+                .HasColumnType("SMALLINT")
+                .HasColumnName("Ano_Modelo");
+
+            builder
+                .Property(c => c.AnoFabricacao)
+                .IsRequired()
+                .HasColumnType("SMALLINT")
+                .HasColumnName("Ano_Fabricação");
+
+            builder
+                .Property(c => c.Chassi)
+                .IsRequired()
+                .HasColumnType("VARCHAR");
+
+            builder
+                .Property(c => c.Preco)
+                .IsRequired()
+                .HasColumnType("FLOAT");
         }
     }
 }
