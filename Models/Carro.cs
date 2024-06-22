e7ec7171d8d635aeb0e26c02b39fb7a3c3a1a6d5
eduardocaas <eduardofc.dev@gmail.com>
2024-06-22 00:04:22 -0300

    Mensagem pt-br para MaxLength

--- Models/Carro.cs
+++ Models/Carro.cs
@@ -5,10 +5,10 @@ namespace CarCenter.Models
     public class Carro
     {
         public int Id { get; set; }
-        [MaxLength(80, ErrorMessage = "Marca muito longa")]
+        [MaxLength(80, ErrorMessage = "MaxLength")]
         [Required(ErrorMessage = "Required")]
         public string Marca { get; set; }
-        [MaxLength(80, ErrorMessage = "Modelo muito longo")]
+        [MaxLength(80, ErrorMessage = "MaxLength")]
         [Required(ErrorMessage = "Required")]
         public string Modelo { get; set; }
         [Display(Name = "Ano do Modelo")]
@@ -17,7 +17,7 @@ namespace CarCenter.Models
         [Display(Name = "Ano de Fabricação")]
         [Required(ErrorMessage = "Required")]
         public int AnoFabricacao { get; set; }
-        [MaxLength(80, ErrorMessage = "Chassi muito longo")]
+        [MaxLength(80, ErrorMessage = "MaxLength")]
         [Required(ErrorMessage = "Required")]
         public string Chassi { get; set; }
         [DisplayFormat(DataFormatString = "{0:#,##0.00}")]
