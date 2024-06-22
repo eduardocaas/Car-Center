8e54c8cec7ba13ecda347ea6910d9b573a77e804
eduardocaas <eduardofc.dev@gmail.com>
2024-06-21 23:31:29 -0300

    Adiconada validações de tamanho

--- Models/Carro.cs
+++ Models/Carro.cs
@@ -5,12 +5,15 @@ namespace CarCenter.Models
     public class Carro
     {
         public int Id { get; set; }
+        [MaxLength(80, ErrorMessage = "Marca muito longa")]
         public string Marca { get; set; }
+        [MaxLength(80, ErrorMessage = "Modelo muito longo")]
         public string Modelo { get; set; }
         [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
         [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
+        [MaxLength(80, ErrorMessage = "Chassi muito longo")]
         public string Chassi { get; set; }
         [DisplayFormat(DataFormatString = "{0:#,##0.00}")]
         public float Preco { get; set; }
