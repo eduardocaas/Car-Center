4a30d6b267dfa15281c0e013ca5beecb8d5ca46b
eduardocaas <eduardofc.dev@gmail.com>
2024-06-21 22:43:32 -0300

    Adicionadas anotações Display

--- Models/Carro.cs
+++ Models/Carro.cs
@@ -1,13 +1,18 @@
-﻿namespace CarCenter.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarCenter.Models
 {
     public class Carro
     {
         public int Id { get; set; }
         public string Marca { get; set; }
         public string Modelo { get; set; }
+        [Display(Name = "Ano do Modelo")]
         public int AnoModelo { get; set; }
+        [Display(Name = "Ano de Fabricação")]
         public int AnoFabricacao { get; set; }
         public string Chassi { get; set; }
+        [DisplayFormat(DataFormatString = "{0:#,##0.00}")]
         public float Preco { get; set; }
     }
 }
