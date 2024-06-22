4572a98e1a791dcde07b0da16a33e2c23f6424cf
eduardocaas <eduardofc.dev@gmail.com>
2024-06-21 22:21:20 -0300

    Adicionado DbSets

--- Data/CarCenterContext.cs
+++ Data/CarCenterContext.cs
@@ -1,6 +1,17 @@
-﻿namespace CarCenter.Data
+﻿using CarCenter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarCenter.Data
 {
-    public class CarCenterContext
+    public class CarCenterContext : DbContext
     {
+        public CarCenterContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Nota> Notas { get; set; }
+        public DbSet<Vendedor> Vendedores { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Carro> Carros { get; set; }
+
     }
 }
