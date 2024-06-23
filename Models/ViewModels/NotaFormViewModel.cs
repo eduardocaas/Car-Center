5ae2f8d9e5b22f9bba574b231c3cd8e6d122774b
eduardocaas <eduardofc.dev@gmail.com>
2024-06-22 23:44:13 -0300

    Adicionadas propriedades

--- Models/ViewModels/NotaFormViewModel.cs
+++ Models/ViewModels/NotaFormViewModel.cs
@@ -2,5 +2,9 @@
 {
     public class NotaFormViewModel
     {
+        public Nota Nota { get; set; }
+        public List<Vendedor> Vendedores { get; set; }
+        public List<Cliente> Clientes { get; set; }
+        public List<Carro> Carros { get; set; }
     }
 }
