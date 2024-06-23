cfe17b16a2d50d6267bd6b68eaf1e540c61028fc
eduardocaas <eduardofc.dev@gmail.com>
2024-06-22 22:59:26 -0300

    Adicionada mensagens customizaveis

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -6,26 +6,26 @@ namespace CarCenter.Models
     public class Cliente
     {
         public int Id { get; set; }
-        [Required]
-        [MaxLength(80)]
+        [Required(ErrorMessage = "Required")]
+        [MaxLength(80, ErrorMessage = "MaxLength")]
         public string Nome { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
         [DataType(DataType.Date)]
         [Display(Name = "Data de Nascimento")]
         public DateTime DataNascimento { get; set; }
         [EmailAddress]
-        [Required]
-        [MaxLength(80)]
+        [Required(ErrorMessage = "Required")]
+        [MaxLength(80, ErrorMessage = "MaxLength")]
         public string Email { get; set; }
-        [Required]
-        [MaxLength(11)]
+        [Required(ErrorMessage = "Required")]
+        [MaxLength(11, ErrorMessage = "MaxLength")]
         public string Telefone { get; set; }
-        [Required]
-        [MaxLength(200)]
+        [Required(ErrorMessage = "Required")]
+        [MaxLength(200, ErrorMessage = "MaxLength")]
         public string Endereco { get; set; }
         [CpfValidation(errorMessage: "Insira um CPF válido")]
-        [Required]
-        [MaxLength(11)]
+        [Required(ErrorMessage = "Required")]
+        [MaxLength(11, ErrorMessage = "MaxLength")]
         public string CPF { get; set; }
     }
 }
