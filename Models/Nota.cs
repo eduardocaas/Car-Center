eebedd6e511370c7197d1cbdb979444771473c35
eduardocaas <eduardofc.dev@gmail.com>
2024-06-22 22:50:25 -0300

    Adicionada validações

--- Models/Nota.cs
+++ Models/Nota.cs
@@ -1,10 +1,18 @@
-﻿namespace CarCenter.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarCenter.Models
 {
     public class Nota
     {
         public int Id { get; set; }
+        [Required]
         public int Numero { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Emissão")]
         public DateTime DataEmissao { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime Garantia { get; set; }
         public float ValorVenda { get; set; }
 
--- Models/Vendedor.cs
+++ Models/Vendedor.cs
@@ -1,11 +1,21 @@
-﻿namespace CarCenter.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarCenter.Models
 {
     public class Vendedor
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(80)]
         public string Nome { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Admissão")]
         public DateTime DataAdmissao { get; set; }
+        [Required]
+        [MaxLength(80)]
         public string Matricula { get; set; }
+        [Required]
         public float Salario { get; set; }
 
         public float CalculoComissao(float ValorVenda)
