b35501c73ee0301995b16f97a3e635b667f5808c
eduardocaas <eduardofc.dev@gmail.com>
2024-06-22 23:48:49 -0300

    Adicionadas annotations Display

--- Models/Nota.cs
+++ Models/Nota.cs
@@ -15,17 +15,21 @@ namespace CarCenter.Models
         [DataType(DataType.Date)]
         public DateTime Garantia { get; set; }
         [Required(ErrorMessage = "Required")]
+        [Display(Name = "Valor da venda")]
         public float ValorVenda { get; set; }
 
         [Required(ErrorMessage = "Required")]
+        [Display(Name = "Nome do comprador")]
         public int CompradorId { get; set; }
         public Cliente Comprador { get; set; }
 
         [Required(ErrorMessage = "Required")]
+        [Display(Name = "Nome do vendedor")]
         public int VendedorId { get; set; }
         public Vendedor Vendedor { get; set; }
 
         [Required(ErrorMessage = "Required")]
+        [Display(Name = "Chassi do carro")]
         public int CarroId { get; set; }
         public Carro Carro { get; set; }
     }
