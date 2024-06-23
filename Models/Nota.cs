dff1d5481a024fa82c6a49943e4f011557b5ff03
eduardocaas <eduardofc.dev@gmail.com>
2024-06-23 16:00:38 -0300

    Ajuste de validacoes e campos anulaveis

--- Models/Nota.cs
+++ Models/Nota.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarCenter.Models
 {
@@ -7,12 +9,11 @@ namespace CarCenter.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "Required")]
         public int Numero { get; set; }
-        [Required(ErrorMessage = "Required")]
-        [DataType(DataType.Date)]
+        [ValidateNever]
         [Display(Name = "Emissão")]
-        public DateTime DataEmissao { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime Garantia { get; set; }
+        public DateTime? DataEmissao { get; set; }
+        [ValidateNever]
+        public DateTime? Garantia { get; set; }
         [Required(ErrorMessage = "Required")]
         [Display(Name = "Valor da venda")]
         public float ValorVenda { get; set; }
@@ -20,16 +21,19 @@ namespace CarCenter.Models
         [Required(ErrorMessage = "Required")]
         [Display(Name = "CPF do comprador")]
         public int CompradorId { get; set; }
+        [ValidateNever]
         public Cliente Comprador { get; set; }
 
         [Required(ErrorMessage = "Required")]
         [Display(Name = "Matrícula do vendedor")]
         public int VendedorId { get; set; }
+        [ValidateNever]
         public Vendedor Vendedor { get; set; }
 
         [Required(ErrorMessage = "Required")]
         [Display(Name = "Chassi do carro")]
         public int CarroId { get; set; }
+        [ValidateNever]
         public Carro Carro { get; set; }
     }
 }
