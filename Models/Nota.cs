d9b6233a4cfa67057a2e14938ea1471b26d3fbe5
eduardocaas <eduardofc.dev@gmail.com>
2024-06-22 23:01:39 -0300

    Adicionada mensagens customizaveis

--- Models/Nota.cs
+++ Models/Nota.cs
@@ -5,23 +5,27 @@ namespace CarCenter.Models
     public class Nota
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
         public int Numero { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
         [DataType(DataType.Date)]
         [Display(Name = "Emissão")]
         public DateTime DataEmissao { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
         [DataType(DataType.Date)]
         public DateTime Garantia { get; set; }
+        [Required(ErrorMessage = "Required")]
         public float ValorVenda { get; set; }
 
+        [Required(ErrorMessage = "Required")]
         public int CompradorId { get; set; }
         public Cliente Comprador { get; set; }
 
+        [Required(ErrorMessage = "Required")]
         public int VendedorId { get; set; }
         public Vendedor Vendedor { get; set; }
 
+        [Required(ErrorMessage = "Required")]
         public int CarroId { get; set; }
         public Carro Carro { get; set; }
     }
--- Models/Vendedor.cs
+++ Models/Vendedor.cs
@@ -5,17 +5,17 @@ namespace CarCenter.Models
     public class Vendedor
     {
         public int Id { get; set; }
-        [Required]
-        [MaxLength(80)]
+        [Required(ErrorMessage = "Required")]
+        [MaxLength(80, ErrorMessage = "MaxLength")]
         public string Nome { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
         [DataType(DataType.Date)]
         [Display(Name = "Data de Admissão")]
         public DateTime DataAdmissao { get; set; }
-        [Required]
-        [MaxLength(80)]
+        [Required(ErrorMessage = "Required")]
+        [MaxLength(80, ErrorMessage = "MaxLength")]
         public string Matricula { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
         public float Salario { get; set; }
 
         public float CalculoComissao(float ValorVenda)
